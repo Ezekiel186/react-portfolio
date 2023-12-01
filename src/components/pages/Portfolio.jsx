b36058356fd6e0363959255d70591be6dccee6c9
Ezekiel <ezekieljamolin186@gmail.com>
2023-11-30 21:32:03 -0800

    changed styling and added portfolio page content

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,7 +1,41 @@
 export default function Portfolio() {
     return (
       <div>
-        <h1>Portfolio</h1>
+         <section class="project-container">
+            <article id="Project-1">
+                <a href="https://jacastro619.github.io/Pick4Me/?"> <img src="public/images/pick4me.png" alt="pick4me-home-page"/>
+                    <p>Pick4Me</p>
+                </a>
+            </article>
+            <div class="card0">
+            <article id="Project">
+                <a href="https://majority-matters-080972dae02f.herokuapp.com/"><img src="public/images/majority-matters.png" alt="majority-matters-home-page"/>
+                    <p>Majority Matters</p>
+                </a>
+            </article>
+        </div>
+        <div class="card0">
+            <article id="Project">
+                <a href="#"><img src="https://www.drupal.org/files/project-images/spinner.png" alt="loading-image"/>
+                    <p>Project-3</p>
+                </a>
+            </article>
+        </div>
+        <div class="card0">
+            <article id="Project">
+                <a href="https://ezekiel186.github.io/challenge-6-serverside-apis/"><img src="public/images/weather-api.png" alt="weather-api-website-image"/>
+                    <p>Weather Api</p>
+                </a>
+            </article>
+        </div>
+        <div class="card0">
+            <article id="Project">
+                <a href="#"><img src="https://www.drupal.org/files/project-images/spinner.png" alt="loading-image"/>
+                    <p>Project-5</p>
+                </a>
+            </article>
+        </div>
+        </section>
       </div>
     );
   }  
