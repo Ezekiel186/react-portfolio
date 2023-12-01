77830613952480f8ab0a0aacd9aa9bca9cd63ca3
Ezekiel <ezekieljamolin186@gmail.com>
2023-11-30 20:54:37 -0800

    added css

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,7 +1,17 @@
 export default function About() {
     return (
       <div>
-        <h1>About Me</h1>
+        <section className="me-container">
+        <div className="me">
+                <img src="/public/images/profile.jpg" alt="Ezekiel-Jamolin-Photo"/>
+                <div className="text-container">
+                <div className="text">
+                <h1>Hi I'm Ezekiel Jamolin,</h1>
+                <h2>I am 18 and I am a student at the UC Berkeley Extension Full Stack Coding Bootcamp. I am working hard to pursue a rewarding career in the tech industry.</h2>
+                </div>
+            </div>
+            </div>
+            </section>
       </div>
     );
   }
