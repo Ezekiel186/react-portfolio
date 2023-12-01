fae25e35345d4b84616825b42801a28e986c184b
Ezekiel <ezekieljamolin186@gmail.com>
2023-12-01 14:53:44 -0800

    made functioning contact form with css

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,8 +1,76 @@
-export default function Contact() {
-    return (
-      <div>
-        <h1>Contact</h1>
-      </div>
-    );
-  }
-  
+import React, { useState } from 'react';
+
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const { name, email, message } = formData;
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!message.trim()) {
+      alert('Please enter a message before submitting.');
+      return;
+    }
+    setFormData({
+      name: '',
+      email: '',
+      message: '',
+    });
+
+    alert('Form submitted successfully!');
+  };
+
+  return (
+    <div className='contact'>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={message}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button className='submit-btn' type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
